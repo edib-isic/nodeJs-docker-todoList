b4e95099e97a72df16e75cf59175f4146048630f
Ulrich Giorgio Jaeger <edib.isic@gmail.com>
2021-08-06 10:30:16 +0200

    nice to be such a genious general

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const db = require('./persistence');
+const getItems = require('./routes/getItems');
+const addItem = require('./routes/addItem');
+const updateItem = require('./routes/updateItem');
+const deleteItem = require('./routes/deleteItem');
+
+app.use(require('body-parser').json());
+app.use(express.static(__dirname + '/static'));
+
+app.get('/items', getItems);
+app.post('/items', addItem);
+app.put('/items/:id', updateItem);
+app.delete('/items/:id', deleteItem);
+
+db.init()
+    .then(() => {
+        app.listen(5353, '127.0.0.1',() => console.log('Listening on port 54778'));
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit());
+};
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
